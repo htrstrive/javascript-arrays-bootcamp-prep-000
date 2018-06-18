1a73a1ccc26370fc60984ff91c69f9f5eaad06af
Han Tran <htrstrive@gmail.com>
2018-06-18 17:43:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
-function destructiveAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
