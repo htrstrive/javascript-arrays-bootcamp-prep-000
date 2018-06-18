60a26154ca482c9a7077f0d85745190bd5a4e5d9
Han Tran <htrstrive@gmail.com>
2018-06-18 17:40:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+function destructiveAddElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
