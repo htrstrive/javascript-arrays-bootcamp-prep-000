285b5bd2200fac6502de39e7dd14a26d481c2381
Han Tran <htrstrive@gmail.com>
2018-06-18 18:13:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
   }
-  function destructivelyRemoveElementFromEndOfArray(array){
+function destructivelyRemoveElementFromEndOfArray(array){
     array.pop()
     return array
   }
+function removeElementFromEndOfArray(array){
+  return array.slice
+}
+  
